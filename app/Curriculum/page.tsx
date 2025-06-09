26ec4f91247db66a0cebb2f3aef5f3288a957783
JoseSaul09 <jose.avina5548@alumnos.udg.mx>
2025-06-09 12:48:44 -0600

    Respaldo HomePage

--- app/Curriculum/page.tsx
+++ app/Curriculum/page.tsx
@@ -1,20 +1,17 @@
-"use client";
-import { useEffect } from 'react'
-import { Document, Page, pdfjs } from 'react-pdf'
-import 'react-pdf/dist/esm/Page/AnnotationLayer.css'
+'use client'; // si estás en Next.js App Router
 
-// Configura la ruta del worker
-pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`
+import { pdfjs, Document, Page } from 'react-pdf';
+import 'react-pdf/dist/esm/Page/AnnotationLayer.css';
 
-export default function Curriculum(){
-    return(
-        <div className="flex flex-col items-center justify-center min-h-screen p-8 bg-secundario">
-            <h1 className="text-3xl font-bold mb-6">Mi Currículum</h1>
-            <div className="border shadow-lg">
-                <Document file="/Curriculum.pdf">
-                <Page pageNumber={1} />
-                </Document>
-            </div>
-        </div>
-    );
+pdfjs.GlobalWorkerOptions.workerSrc = '/pdf.worker.min.js';
+
+export default function PDFViewer() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen p-8 bg-secundario">
+    <h1 className="text-3xl font-bold mb-6">Mi Currículum</h1>
+      <Document file="/docs/Curriculum.pdf" onLoadError={console.error}>
+        <Page pageNumber={1} />
+      </Document>
+    </div>
+  );
 }
