49a1e02bd9dff610ed8e7d2f8b587cfd21c11e0b
JoseSaul09 <jose.avina5548@alumnos.udg.mx>
2025-06-09 13:16:27 -0600

    Version principal de respaldo solo con HomePage

--- app/Curriculum/page.tsx
+++ app/Curriculum/page.tsx
@@ -1,17 +1,10 @@
 'use client'; // si estás en Next.js App Router
 
-import { pdfjs, Document, Page } from 'react-pdf';
-import 'react-pdf/dist/esm/Page/AnnotationLayer.css';
-
-pdfjs.GlobalWorkerOptions.workerSrc = '/pdf.worker.min.js';
-
 export default function PDFViewer() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen p-8 bg-secundario">
     <h1 className="text-3xl font-bold mb-6">Mi Currículum</h1>
-      <Document file="/docs/Curriculum.pdf" onLoadError={console.error}>
-        <Page pageNumber={1} />
-      </Document>
+      
     </div>
   );
 }
